c6654be4dccad32a80d3e49c97debdacd8374029
Brendan Grubb and Jayeson Kiyabu <jayeson@>
2017-02-09 14:43:42 -0800

    set up initial files

---
+++ app/app.php
@@ -0,0 +1,15 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__.'/../vendor/autoload.php';
+
+    $app = new Silex\Application();
+
+    $app->get('/', function() {
+
+        return "hello Jayeson";
+
+
+    });
+
+    return $app;
+?>
